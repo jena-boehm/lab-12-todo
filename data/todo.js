e49e375dd0f7f41ed6cbeb426ca89507d158112d
Jena Boehm <boehmjena@gmail.com>
2020-11-10 16:51:36 -0800

    initial commit

---
+++ data/todo.js
@@ -0,0 +1,32 @@
+module.exports = [
+  {
+    id: 1,
+    todo: 'wash the dishes',
+    completed: false,
+  },  
+  {
+    id: 2,
+    todo: 'clean the bathroom',
+    completed: false,
+  },
+  {
+    id: 3,
+    todo: 'fold the laundry',
+    completed: false,
+  },
+  {
+    id: 4,
+    todo: 'take out the trash',
+    completed: false,
+  },
+  {
+    id: 5,
+    todo: 'stretch',
+    completed: false,
+  },
+  {
+    id: 6,
+    todo: 'do my homework',
+    completed: false,
+  }
+];
